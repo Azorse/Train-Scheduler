cb7f4352b68ebc3361d5ed27eb9cfc2259a55013
Dan Schissler <schisslerdg@gmail.com>
2019-11-15 12:00:30 -0800

    functional with bug

--- javascript/app.js
+++ javascript/app.js
@@ -1,4 +1,4 @@
-$(document).ready(function(){
+$(document).ready(function() {
   var firebaseConfig = {
     apiKey: "AIzaSyBR4ER8Nlitz47jp6_48U3gkF6t-JrUkgE",
     authDomain: "timestampproject.firebaseapp.com",
@@ -11,50 +11,74 @@ $(document).ready(function(){
   // Initialize Firebase
   firebase.initializeApp(firebaseConfig);
 
-
   var database = firebase.database();
 
-  $('#add-train').on('click', function(event) {
-      event.preventDefault();
-      name = $('#trainName').val().trim();
-      destination = $('#trainDestination').val().trim();
-      firstTrain = $('#trainTime').val().trim();
-      frequency = $('#trainFrequency').val().trim();
-                        
-      database.ref().push({
-          name: name,
-          destination: destination,
-          //firstTrain: firstTrain,//store as unix, large x
-          frequency: frequency
-      })
+  $("#add-train").on("click", function(event) {
+    event.preventDefault();
+    name = $("#trainName")
+      .val()
+      .trim();
+    destination = $("#trainDestination")
+      .val()
+      .trim();
+    firstTrain = $("#trainTime")
+      .val()
+      .trim();
+    frequency = parseInt(
+      $("#trainFrequency")
+        .val()
+        .trim()
+    );
 
-      // var startingDate = childSnapshot.val().startDate 
-
-      database.ref().on('child_added', function(childSnapshot){
-          var startingDate = childSnapshot.val().startDate;
-
-          console.log(childSnapshot.val().name)
-          console.log(childSnapshot.val().destination)
-          console.log(childSnapshot.val().firstTrain)
-          console.log(childSnapshot.val().frequency)
-          //console.log(startingDate.diff(moment(), "months")); FIRST TRAIN
-
-          $('#new-trains').append(`
-          <tr>
-              <td>${childSnapshot.val().name}</td>
-              <td>${childSnapshot.val().destination}</td>
-              <td>${childSnapshot.val().frequency}</td>
-
-          </tr>`)
-          //Need to add next arrival and minutes away
-
-        }, function(errorObject){
-          console.log("Child added failed: " + errorObject.code);
-        });
-
-
+    database.ref().push({
+      name: name,
+      destination: destination,
+      firstTrain: firstTrain,
+      frequency: frequency
     });
 
+    database.ref().on(
+      "child_added",
+      function(childSnapshot) {
+        var trainInput = childSnapshot.val();
 
+        var initalTrain = moment(trainInput.firstTrain, "HH:mm");
+        var presentTime = moment();
+        var nextArrival;
+        var minutesAway;
 
-})
+        if (presentTime < initalTrain) {
+          nextArrival = initalTrain.format("HH:mm");
+          minutesAway = moment(initalTrain).fromNow("m");
+
+        } else {
+          var a = moment(presentTime, "HH:mm");
+          var b = moment(initalTrain, "HH:mm");
+
+          var timePassed = a.diff(b, "minutes");
+          var missedTrains = Math.round(timePassed / trainInput.frequency);
+
+          nextArrival = moment(b).add(
+            (missedTrains + 1) * trainInput.frequency,
+            "minutes"
+          );
+          var minutesPast = timePassed % trainInput.frequency;
+          minutesAway = trainInput.frequency - minutesPast;
+        }
+
+        $("#new-trains").append(
+          `<tr>
+            <td>${trainInput.name}</td>
+            <td>${trainInput.destination}</td>
+            <td>${trainInput.frequency}</td>
+            <td>${nextArrival}</td>
+            <td>${minutesAway}</td>
+          </tr>`
+        );
+      },
+      function(errorObject) {
+        console.log("Child added failed: " + errorObject.code);
+      }
+    );
+  });
+});
